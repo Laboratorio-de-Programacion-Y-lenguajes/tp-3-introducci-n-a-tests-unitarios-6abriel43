cdf734906d0861c05ffa7fa177458f4a76f8472e
6abriel43 <gabrielguitian202@gmail.com>
2026-04-15 21:40:46 -0300

    test: agrego @pytest.mark.parametrize y valores para los casos de resta pedidos y funcion para el calculo

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -19,3 +19,15 @@ def test_sub_resta_positivos():
 #   - Restar dos números decimales (float)
 #
 # Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
+
+@pytest.mark.parametrize(
+"a,b,expected",     
+[
+(4, 5, -1),         # Restar un número mayor al primero
+(8, 0, 8),          # Restar cero
+(-3, -3, 6),        # Restar dos números negativos
+(5.2, 2.9, 2.3)     # Restar dos números decimales
+]
+)
+def test_sub_parametrizado(a, b, expected):
+    assert sub(a, b) == pytest.approx(expected)
